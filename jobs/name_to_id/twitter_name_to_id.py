78564f106ed71f9b58ba88125d6f6e2f4f54b8c7
Bruce Spang <bruce@brucespang.com>
2011-06-25 15:25:37 -0400

    Added mrjob

--- jobs/name_to_id/twitter_name_to_id.py
+++ jobs/name_to_id/twitter_name_to_id.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python
 
 import os, sys
+from mrjob.job import MRJob
 
 sys.path.append("deps/httplib2-0.7.1")
 sys.path.append("deps/python-oauth2")
 sys.path.append("deps/simplejson-2.1.6")
 sys.path.append("deps/python-twitter-0.8.2")
 
-import twitter
+from deps.python_twitter import twitter
 
-api = twitter.Api()
+class MRNameToId(MRJob):
+  def run(self):
+    return[self.mr(self.get_ids, self.reduce),]
 
-# twitter url => twitter id
+  def get_ids():
+    api = twitter.Api()
+    artist_twitterid = []
 
-artist_twitterid = []
+    for line in sys.stdin:
+        line = line.strip()
+        id, artist, profile = line.split('\t')
+        username = profile.split('/')[-1]
+        tid = api.GetUser(username).id
+        artist_twitterid.append((artist, username, tid))
 
-for line in sys.stdin:
-    line = line.strip()
-    id, artist, profile = line.split('\t')
-    username = profile.split('/')[-1]
-    tid = api.GetUser(username).id
-    artist_twitterid.append((artist, username, tid))
+    for artist, username, tid in artist_twitterid:
+        print '%s\t%s\t%s'% (artist, username, tid)
 
-for artist, username, tid in artist_twitterid:
-    print '%s\t%s\t%s'% (artist, username, tid)
-
+if __name__ == '__main__':
+  MRNameToId.run()
